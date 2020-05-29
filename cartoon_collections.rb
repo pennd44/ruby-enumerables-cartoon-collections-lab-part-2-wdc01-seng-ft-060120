f9e3c52e0a3f8e101ec28b7de4ec8b3fbb3c4cb6
Penn M Diba <pdiba@oxbowschool.org>
2020-05-29 17:21:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,6 +28,5 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.find do |call|
     valid_calls.include?(call)
-    return call
   end
 end
